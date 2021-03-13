790b380b892e552bb01a2f638da5e533e693acf5
MoisesTapia <moisestapia741@gmail.com>
2021-03-12 23:15:37 -0600

    New output with more information

--- hackslabs.py
+++ hackslabs.py
@@ -8,10 +8,11 @@ import argparse as argp
 from art import *
 from colorama import Fore, init, Back, Style
 import sys
-#from rich.console import Console
-#from rich.table import Column, Table
 
-init()
+VERDE = Fore.LIGHTGREEN_EX
+RRED = Fore.LIGHTRED_EX
+CYYAN = RRED =Fore.LIGHTCYAN_EX
+RESETT = Fore.RESET
 
 #
 # ami-050184d2d97a1193f us-west-1
@@ -21,61 +22,54 @@ init()
 
 client = b3.client('ec2')
 
-#console = Console()
 
-def parsearguments():
-    
-    parser = argp.ArgumentParser(
-        description=__doc__, 
-        formatter_class=argp.RawDescriptionHelpFormatter)
-    
-    parser.add_argument("-z", "--awstype", dest="size", 
-                        help="""choose the sice of your vm in aws, types: 
-                        t2.micro, t2.small, t2.medium, t2.large, t2.xlarge.
-                        Remember visit aws.com to see the cost for each vm""")
-    
-    parser.add_argument("-mx", "--maxvm", dest="maxvm",
-                        help="Max number of the same instances",
-                        type=int)
-    
-    parser.add_argument("-mn", "--minvm", dest="minvm",
-                        help="Min number of the same instances default 1",
-                        default=1,
-                        type=int)
-    
-    parser.add_argument( "-k", "--keypair", dest="keys",
-                        default='KaliLinux',
-                        help="Name of your Key Pair in AWS (ssh keys)")
-    
-    parser.add_argument("-l", "--launch",dest="launch",
-                        default="aws",
-                        help="""
-                        This option requires the next attr:
-                             --awstype, --maxvm, --minvm, --keypair
-                        """)
-    
-    parser.add_argument("--stop", dest="stop",
-                        help="Stop the instance or instances")
 
-    parser.add_argument("-s", "--start", dest="start",
-                        help="Start the instance or instances")
+parser = argp.ArgumentParser(
+    description=__doc__, 
+    formatter_class=argp.RawDescriptionHelpFormatter)
 
-    parser.add_argument("-t", "--terminate",dest="terminate",
-                        type=str,
-                        help="""
-                        Terminate the instance or instances this option will delete the instances take care""")
-    
-    parser.add_argument("-ls", "--list", dest="list",
-                        type=str,
-                        default="vms",
-                        help="list all instances in your account")
+parser.add_argument("-z", "--awstype", dest="size", 
+                    help="""choose the sice of your vm in aws, types: 
+                    t2.micro, t2.small, t2.medium, t2.large, t2.xlarge.
+                    Remember visit aws.com to see the cost for each vm""")
 
-    parser.add_argument("-in" , "--getinfo", dest="getinfo",
-                        type=str,
-                        default="vm",
-                        help="get info of your instance")
-    return parser.parse_args()
+parser.add_argument("-mx", "--maxvm", dest="maxvm",
+                    help="Max number of the same instances",
+                    type=int)
 
+parser.add_argument("-mn", "--minvm", dest="minvm",
+                    help="Min number of the same instances default 1",
+                    default=1,
+                    type=int)
+
+parser.add_argument( "-k", "--keypair", dest="keys",
+                    default='KaliLinux',
+                    help="Name of your Key Pair in AWS (ssh keys)")
+
+parser.add_argument("-l", "--launch",dest="launch",
+                    default="aws",
+                    help="""
+                    This option requires the next attr:
+                         --awstype, --maxvm, --minvm, --keypair
+                    """)
+
+parser.add_argument("--stop", dest="stop",
+                    help="Stop the instance or instances")
+parser.add_argument("-s", "--start", dest="start",
+                    help="Start the instance or instances")
+parser.add_argument("-t", "--terminate",dest="terminate",
+                    type=str,
+                    help="""
+                    Terminate the instance or instances this option will delete the instances take care""")
+
+parser.add_argument("-ls", "--list", dest="list",
+                    type=str,
+                    default="vms",
+                    help="list all instances in your account")
+parser.add_argument("-in" , "--getinfo", dest="getinfo",
+                    type=str,
+                    default="vm",
+                    help="get info of your instance")
 
 
 class Instance:
@@ -156,22 +150,23 @@ class Instance:
         aws_terminate = client.terminate_instances(InstanceIds=[self.instanceid])
         
         for instances in aws_terminate['TerminatingInstances']:
-            print("Stop instance ID: {}".format(instances['InstanceId']))
+            print("Stopping instance ID: " + RRED + instances['InstanceId'] + RESETT)
             print("Code: {}".format(instances['CurrentState']['Code']))
-            print("Code: {}".format(instances['CurrentState']['Name']))
+            print("Status: " + CYYAN + instances['CurrentState']['Name'] + RESETT)
     
     def getinfo_instances(self):
         resp = client.describe_instances()
         
-        print(Fore.LIGHTCYAN_EX + "\n\t\tList of Instances and status\n"+ Fore.RESET)
+        tprint('Resume')
+        
+        print(CYYAN + "\n\t\tList of Instances\n"+ RESETT)
         resp = client.describe_instances()
 
         for reservation in resp['Reservations']:
             for instances in reservation['Instances']:
                 print("Id of instances: {}".format(instances['InstanceId']))
-        print(Fore.LIGHTRED_EX + "------" *10 + Fore.RESET)
+        print(RRED + "------" *10 + RESETT)
             
-        print(Fore.LIGHTRED_EX + "\n\t\tBasic Information \n" + Fore.RESET)
         
         for reserved in resp['Reservations']:
             for info in reserved['Instances']:
@@ -182,53 +177,51 @@ class Instance:
                 print(info['LaunchTime'])
                 print(info['Monitoring']['State'] + "\n")
                 
-        print(Fore.LIGHTRED_EX + "------" *10 + Fore.RESET)
+        print(RRED + "------" *10 + Fore.RESET)
         
-        print(Fore.LIGHTBLUE_EX + "\n\t\tPlacement" + Fore.RESET)
         for getinfo2 in resp['Reservations']:
             for info2 in getinfo2['Instances']:
                 
                     print("Availability Zone: {}".format(info2['Placement']['AvailabilityZone']))
-                    #print(info2['Placement']['HostId'])
-                    #print("Affinity: {}".format(info2['Placement']['Affinity']))
-                    print("Group name: {}".format(info['Placement']['GroupName'] + "\n"))
+                    print("Group name: {}".format(info['Placement']['GroupName']))
+                    print("Tenancy: {}".format(info['Placement']['Tenancy']  + "\n"))
         
-        print(resp)
-                    
-        print(Fore.LIGHTRED_EX + "------" *10 + Fore.RESET)
-        print(Fore.LIGHTGREEN_EX + "\n\t\tPlatform\n" + Fore.RESET)
-               
-        for pt in resp['Reservations']:
-            for pt2 in pt['Instances']:
-                
-                print(Fore.LIGHTGREEN_EX + """
-                      Information about the platform:\n
-                      Platform: {}\n
-                      PrivateDNS: {}\n
-                      PrivateIP Address: {}
-                      """.format(pt2['Platform'], pt2['PrivateDnsName'], pt2['PrivateIpAddress']) + Fore.RESET)
+        #print(resp)
         
-        print(Fore.LIGHTRED_EX + "------" *10 + Fore.RESET)
+        print(RRED + "------" *6 + RESETT)
         
-        print(Fore.LIGHTGREEN_EX + "\n\t\tPublic options\n"  + Fore.RESET)
+        print(VERDE + "\n\t\tPublic options\n"  + RESETT)
         for publicinfo in resp['Reservations']:
             for pbl in publicinfo['Instances']:
-                print("Your Public DNSname: {}".format(pbl['PublicDnsName']))
-                print("Your Public IP: {}".format(pbl['PublicIpAddress']))
-                #print(pbl['RamdiskId'])
+                print("Your Public DNSname: " + VERDE + pbl['PublicDnsName'] + RESETT )
+                print("Your Public IP: " + VERDE + pbl['PublicIpAddress']  + RESETT )
                 print("Your Arch: {}".format(pbl['Architecture']))
-                print("State code: {}".format(pbl['State']['Name']))
-                print("State code: {}".format(pbl['State']['Code']))
+                print("State Name: {}".format(pbl['State']['Name']))
+                print("State code: " + VERDE + str(pbl['State']['Code']) + RESETT)
                 print("Subnet ID: {}".format(pbl['SubnetId']))
-        
-        print(Fore.LIGHTRED_EX + "------" *10 + Fore.RESET)
+                print("VPC ID: " + VERDE + pbl['VpcId'] + RESETT)
+                
+                for bdm in pbl['BlockDeviceMappings']:
+                    print("Device Name: " + CYYAN + bdm['DeviceName'] + RESETT)
+                    print(CYYAN + "------" *5 + RESETT)
+
+                for element in pbl['ProductCodes']:
+                    for k in element:
+                        print(k + " : " + element[k])
+                        print(Fore.LIGHTCYAN_EX + "------" *5 + Fore.RESET)
+                        
+                for dbnm in pbl['BlockDeviceMappings']:
+                    for keys, values in dbnm['Ebs'].items():
+                        print(keys + " : " + str(values))
+                        print(Fore.LIGHTCYAN_EX + "------" *5 + Fore.RESET)
+    
 
 
 def main():
     tprint('hAcksWlabS')
-    print(Fore.GREEN + "\tBy: Moises Tapia\t" + Fore.RESET + Fore.LIGHTGREEN_EX + "Github: https://github.com/MoisesTapia/" + Fore.RESET)
+    print(Fore.GREEN + "\tBy: Moises Tapia\t" + RESETT + VERDE + "Github: https://github.com/MoisesTapia/" + RESETT)
 
-awsargp = parsearguments()
+awsargp = parser.parse_args()
 
 AWSIMAGE    = "ami-050184d2d97a1193f" 
 AWSTYPE     = awsargp.size # t2.medium
@@ -240,14 +233,14 @@ AWSKEYPAIR  = awsargp.keys
 awsintances = Instance(AWSIMAGE, AWSTYPE, AWSMAX, AWSMIN, AWSKEYPAIR)
 
 if len(sys.argv) < 2:
-    print(
+    print(Fore.LIGHTGREEN_EX + 
     """
     usage mode: hackslabs.py [-h] [-z SIZE] [-mx MAXVM] [-mn MINVM] [-k KEYS]
                     [-l LAUNCH] [--stop STOP] [-s START] [-t TERMINATE]
                     [-ls LIST] [-in GETINFO]
 
     """
-    )
+     + RESETT)
     sys.exit(1)
 
 
